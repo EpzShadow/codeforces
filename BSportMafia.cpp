1bc64510dc37f2752afd505398d05fb21f39f9ca
EpzShadow <85049418+EpzShadow@users.noreply.github.com>
2021-07-25 04:38:32 -0400

    Add files via upload

---
+++ BSportMafia.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main(){
+    float n, k;
+    cin >> n >> k;
+    float root = sqrt(8*k + 8*n + 9);
+    root *= -1.0 /2;
+    float x = -1.0/2 * sqrt(8.0*k + 8.0*n + 9.0) + n + 3.0/2;
+    int t = x;
+    cout << t;
+    //cout << ans << endl;
+
+}
